72f06e711bd1f7684bf3f384f46e43793f75cadc
tarlepp <tarmo.leppanen@protacon.com>
2016-11-04 20:24:14 +0200

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,9 @@
-import { Component, Output } from '@angular/core';
-import { ChannelService, ChannelEvent } from './shared/services/channel'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [ChannelService]
 })
 
-export class AppComponent {
-  channel: ChannelService
-  public data: string = "ei vielä"
-
-  constructor(channel: ChannelService) {
-    this.channel = channel;
-
-    channel.sub("marjatanpuhelin").subscribe((x: ChannelEvent) => {
-      this.data = x.Name
-    })
-  }
- 
-  clicked() {
-    let event: ChannelEvent = {
-      Name: "gaggaa",
-      ChannelName: "marjatanpuhelin",
-      Data: "abc",
-      Json: "jee",
-      Timestamp: undefined
-    }
-
-    this.channel.publish(event);
-  }
-}
+export class AppComponent { }
